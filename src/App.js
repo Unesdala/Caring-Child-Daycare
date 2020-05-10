e0129906aa82a93ec9893ec1f2c8904c3abe760f
Rhayven <codingforllamas@gmail.com>
2020-05-09 20:13:45 -0400

    Added Our Centers page and updated navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Nav/Navbar';
 import AppTemplate from './components/App-Main';
+import DefaultOurCenters from './containers/Centers/OurCenters';
 import DefaultAbout from './containers/About/About';
 import DefaultCareers from './containers/Careers/Careers';
 import DefaultJobs from './containers/Jobs/Jobs';
@@ -16,11 +17,12 @@ export class App extends PureComponent {
           <Navbar />
           <div className="siteContent">
             <Switch>
-              <Route exact path="/" component={AppTemplate} />
-              <Route path="/about" component={DefaultAbout} />
-              <Route path="/careers" component={DefaultCareers} />
+              <Route exact path="/daycare" component={AppTemplate} />
+              <Route path="/daycare/centers" component={DefaultOurCenters} />
+              <Route path="/daycare/about" component={DefaultAbout} />
+              <Route path="/daycare/careers" component={DefaultCareers} />
               <Route path="/daycare/job-descriptions" component={DefaultJobs} />
-              <Route path="/contact" component={DefaultContact} />
+              <Route path="/daycare/contact" component={DefaultContact} />
               <Route component={FourOhFour} />
             </Switch>
           </div>
