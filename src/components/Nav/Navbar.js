ff1ff8081b93a8681e8c0c1c0bd0174c45b2344b
Rhayven <codingforllamas@gmail.com>
2020-06-08 17:28:58 -0400

    Fixes navbar breaking

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -20,11 +20,11 @@ export default class Navigation extends Component {
     const { width } = this.state;
     return (
       <>
-        {width <= 768 - 18 || width === 751
+        {width <= 768
           ? (
             <DefaultCollapsableNav />
           ) : (
-            <nav className="menu">
+            <nav className="menu grid">
               <ul className="menu-nav">
                 <li className="nav-item"><NavLink to="/daycare" className="nav-link" exact activeClassName="active">Home</NavLink></li>
                 <li className="nav-item"><NavLink to="/daycare/centers" className="nav-link">Our Centers</NavLink></li>
