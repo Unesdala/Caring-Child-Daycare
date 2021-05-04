7b70510dc18d26127755b321ce9b6c74510c3960
Rhayven <codingforllamas@gmail.com>
2021-05-04 19:18:28 -0400

    Updates jest pretest and removes unnecessary stuff

--- test/jest-pretest.ts
+++ test/jest-pretest.ts
@@ -4,9 +4,11 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 document.body.innerHTML = '<div id="root"></div>';
 
-// window.location = {
-//   ...window.location,
-//   href: 'https://codingforllamas.com/daycare',
-//   reload: jest.fn(),
-//   assign: jest.fn(),
-// };
+Object.defineProperty(window, 'location', { value: { assign: () => { }, reload: () => { } }, writable: true });
+
+window.location = {
+  ...window.location,
+  href: 'https://codingforllamas.com/daycare',
+  reload: jest.fn(),
+  assign: jest.fn(),
+};
