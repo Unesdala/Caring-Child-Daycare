0acc71295a1b746d8877d09c295d41be072b866a
Rhayven <codingforllamas@gmail.com>
2021-05-04 18:35:40 -0400

    Test criteria updates

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config();
+
+module.exports = {
+  launch: {
+    headless: process.env.HEADLESS !== 'false',
+  },
+  server: {
+    setTimeout: 10000,
+    launchTimeout: 150000,
+    command: 'yarn build:prod && node server.js',
+    port: process.env.PORT,
+  },
+};
--- test/jest-pretest.ts
+++ test/jest-pretest.ts
@@ -4,9 +4,9 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 document.body.innerHTML = '<div id="root"></div>';
 
-window.location = {
-  ...window.location,
-  href: 'https://codingforllamas.com/daycare',
-  reload: jest.fn(),
-  assign: jest.fn(),
-};
+// window.location = {
+//   ...window.location,
+//   href: 'https://codingforllamas.com/daycare',
+//   reload: jest.fn(),
+//   assign: jest.fn(),
+// };
