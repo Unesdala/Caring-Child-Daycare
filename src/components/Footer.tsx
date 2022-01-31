5a8b4be96a1a6c075b8b5eecec116d1c6f6fbb2c
Rhayven <codingforllamas@gmail.com>
2022-01-31 01:55:07 -0500

    Working on footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,19 @@ const Footer = (): JSX.Element => (
   <footer className="main-footer">
     <div className="main-footer--locations">
       <h1>Locations</h1>
+      <div className="main-footer--locations_buttons">
+        <button className="main-footer--locations_buttons--vinton">Vinton Center</button>
+        <button className="main-footer--locations_buttons--460">460 Center</button>
+        <button className="main-footer--locations_buttons--parkway">The Parkway Center</button>
+      </div>
     </div>
     <div className="main-footer--social">
-      <h1>Social Media Buttons</h1>
+      <a href="#">
+        <i className="fab fa-facebook" />
+      </a>
+      <a href="#">
+        <i className="fab fa-twitter" />
+      </a>
     </div>
     <div className="main-footer--links">
       <h1>Request A Tour | Contact Us | Privacy Policy</h1>
--- src/containers/Homepage/HomepageContent.tsx
+++ src/containers/Homepage/HomepageContent.tsx
@@ -20,38 +20,45 @@ const HomeContent = (): JSX.Element => (
       <div className="program-grid">
         <div className="program-grid--infants">
           <div className="program-grid--picture"><h1>Insert Picture Here</h1></div>
-          <div className="program-grid--info-box"><h1>Classname Here. IE: Infants, Etc</h1></div>
+          <div className="program-grid--info-box"><h1>Infants</h1></div>
         </div>
         <div className="program-grid--twos">
           <div className="program-grid--picture"><h1>Insert Picture Here</h1></div>
-          <div className="program-grid--info-box"><h1>Classname Here. IE: Infants, Etc</h1></div>
+          <div className="program-grid--info-box"><h1>Twos</h1></div>
         </div>
         <div className="program-grid--preschool">
           <div className="program-grid--picture"><h1>Insert Picture Here</h1></div>
-          <div className="program-grid--info-box"><h1>Classname Here. IE: Infants, Etc</h1></div>
+          <div className="program-grid--info-box"><h1>PreSchool</h1></div>
         </div>
         <div className="program-grid--private-pre-k">
           <div className="program-grid--picture"><h1>Insert Picture Here</h1></div>
-          <div className="program-grid--info-box"><h1>Classname Here. IE: Infants, Etc</h1></div>
+          <div className="program-grid--info-box"><h1>Pre-K</h1></div>
         </div>
         <div className="program-grid--after-school">
           <div className="program-grid--picture"><h1>Insert Picture Here</h1></div>
-          <div className="program-grid--info-box"><h1>Classname Here. IE: Infants, Etc</h1></div>
+          <div className="program-grid--info-box"><h1>After School</h1></div>
         </div>
         <div className="program-grid--summer-camp">
           <div className="program-grid--picture"><h1>Insert Picture Here</h1></div>
-          <div className="program-grid--info-box"><h1>Classname Here. IE: Infants, Etc</h1></div>
+          <div className="program-grid--info-box"><h1>Summer Camp</h1></div>
         </div>
       </div>
     </section>
     <section className="home-main--reviews">
       <h1>Review Title</h1>
-      <blockquote>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime veniam et facilis at ad quo beatae 
-        quae blanditiis sed ut eius cupiditate quasi, vel consequuntur praesentium corrupti reiciendis laborum eveniet.
-      </blockquote> - Some Family
+      <figure>
+        <blockquote>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime veniam et facilis at ad quo beatae 
+            quae blanditiis sed ut eius cupiditate quasi, vel consequuntur praesentium corrupti reiciendis laborum eveniet.
+          </p>
+        </blockquote>
+        <figcaption>
+          - Some Family
+        </figcaption>
+      </figure>
       <br />
-      <button>Read More Reviews</button>
+      <button className="review-button">Read More Reviews</button>
     </section>
   </>
 );
