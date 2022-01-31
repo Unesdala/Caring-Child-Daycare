4f8e3f0232d22a0a19faedbf71126d953ba5478e
Rhayven <codingforllamas@gmail.com>
2022-01-31 03:22:48 -0500

    More style changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ const Footer = (): JSX.Element => (
     <div className="main-footer--locations">
       <h1>Locations</h1>
       <div className="main-footer--locations_buttons">
-        <button className="main-footer--locations_buttons--vinton">Vinton Center</button>
-        <button className="main-footer--locations_buttons--460">460 Center</button>
-        <button className="main-footer--locations_buttons--parkway">The Parkway Center</button>
+        <button className="main-footer--locations_buttons--vinton btn btn-white">Vinton Center</button>
+        <button className="main-footer--locations_buttons--460 btn btn-white">460 Center</button>
+        <button className="main-footer--locations_buttons--parkway btn btn-white">The Parkway Center</button>
       </div>
     </div>
     <div className="main-footer--social">
@@ -17,7 +17,9 @@ const Footer = (): JSX.Element => (
       </a>
     </div>
     <div className="main-footer--links">
-      <h1>Request A Tour | Contact Us | Privacy Policy</h1>
+      <button className="main-footer--links__tour btn btn-white">Request A Tour</button>
+      <button className="main-footer--links__contact btn btn-white">Contact Us</button>
+      <button className="main-footer--links__privacy btn btn-white">Privacy Policy</button>
     </div>
     <div className="main-footer--logo">
       <h1>Logo</h1>
--- src/containers/Homepage/HomepageContent.tsx
+++ src/containers/Homepage/HomepageContent.tsx
@@ -58,7 +58,7 @@ const HomeContent = (): JSX.Element => (
         </figcaption>
       </figure>
       <br />
-      <button className="review-button">Read More Reviews</button>
+      <button className="btn btn--white review-button">Read More Reviews</button>
     </section>
   </>
 );
